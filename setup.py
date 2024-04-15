41afabc1ba58a2e2885ffa1a456b4af3dfc9e441
PrashantBhusewar <prashantbhusewar5@gmail.com>
2024-04-15 23:23:01 +0530

    folder structure

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Red_Wine_Quality",
+    version="0.0.1",
+    author="Prashant",
+    author_email="prashantbhusewar1599@gmail.com",
+    packages=find_packages()
+)
