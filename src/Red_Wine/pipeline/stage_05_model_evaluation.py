487b2ba3808a18ef875d19177b89062ab2d93cb4
PrashantBhusewar <prashantbhusewar5@gmail.com>
2024-04-17 13:52:15 +0530

    model trainer & evaluation

---
+++ src/Red_Wine/pipeline/stage_05_model_evaluation.py
@@ -0,0 +1,28 @@
+from src.Red_Wine.config.configuration import ConfigurationManager
+from src.Red_Wine.components.model_evaluation import ModelEvaluation
+from src.Red_Wine import logger
+
+
+STAGE_NAME = "Model evaluation stage"
+
+
+class ModelEvaluationTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        model_evaluation_config = config.get_model_evaluation_config()
+        model_evaluation_config = ModelEvaluation(config=model_evaluation_config)
+        model_evaluation_config.save_results()
+
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = ModelEvaluationTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
